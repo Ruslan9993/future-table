84a0041cd818256b3b70edfd18841f07171dabbb
Ruslan <sabbath9993@gmail.com>
2021-03-02 20:25:18 +0300

    users

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = ` http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+export const url = ` http://www.filltext.com/?rows=500&id={number|100}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
 
 const getElements = async() => {
   try {
